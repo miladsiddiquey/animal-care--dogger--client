7d9568fb30318f6c34a3fd25a199bf194c82964c
miladsiddiquey <75581636+miladsiddiquey@users.noreply.github.com>
2021-04-18 19:44:04 +0600

    firebase code change

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDX9X8qHeMfauHHJXiHhll51BcQni4Zio0",
-    authDomain: "rm-food.firebaseapp.com",
-    projectId: "rm-food",
-    storageBucket: "rm-food.appspot.com",
-    messagingSenderId: "17711012356",
-    appId: "1:17711012356:web:a41b866ba295418fd43351"
-  };
+  apiKey: "AIzaSyDbGH768XIH2J9er5cQc9Ia8SgYoSl3N2Y",
+  authDomain: "dog-care-6c0d8.firebaseapp.com",
+  projectId: "dog-care-6c0d8",
+  storageBucket: "dog-care-6c0d8.appspot.com",
+  messagingSenderId: "365818206171",
+  appId: "1:365818206171:web:29cd189d00a24a46aff9fe"
+};
     
     export default firebaseConfig;
